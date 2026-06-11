64f0372ad9a55d1dcfb918b961dec7509c76cded
Vasu-M007 <vasumahajan343@gmail.com>
2026-06-12 03:11:57 +0530

    Added Maked Multi Head Attention Class

---
+++ Masked_MHA.py
@@ -0,0 +1,66 @@
+from Positional_encodings import PositionalEncodings
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+token_info = torch.load('token_info_dict_fr.pt')
+token_ids = torch.load('train_fr_ids.pt')
+
+class MaskedMHA(nn.Module):
+    def __init__(self, num_heads : int, hidden_dim : int, seq_length : int):
+        super().__init__()
+
+        assert hidden_dim % num_heads == 0
+        self.head_dim = hidden_dim // num_heads
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+
+        self.multihead_W_query = nn.Parameter(torch.empty(num_heads, hidden_dim, self.head_dim))
+        self.multihead_W_key = nn.Parameter(torch.empty(num_heads, hidden_dim, self.head_dim))
+        self.multihead_W_value = nn.Parameter(torch.empty(num_heads, hidden_dim, self.head_dim))
+        self.w_o = nn.Parameter(torch.empty(self.head_dim * num_heads, self.hidden_dim))
+        self.mask_matrix = torch.triu(torch.ones(seq_length,seq_length),diagonal=1).bool()
+        self.register_buffer(
+            "masked_matrix",
+            torch.triu(torch.ones(seq_length, seq_length),diagonal=1).bool()
+        )
+        self.parameter_reset()
+        
+    def parameter_reset(self):
+        nn.init.xavier_uniform_(self.multihead_W_query)
+        nn.init.xavier_uniform_(self.multihead_W_key)
+        nn.init.xavier_uniform_(self.multihead_W_value)
+        nn.init.xavier_uniform_(self.w_o)
+
+    def forward(self, input_batch):
+        contexts = []
+        for head in range(self.num_heads):
+            Q = input_batch @ self.multihead_W_query[head]
+            K = input_batch @ self.multihead_W_key[head]
+            V = input_batch @ self.multihead_W_value[head]
+
+            scores = Q.matmul(K.transpose(-1,-2))
+            scores = scores.masked_fill(self.mask_matrix,float("-inf"))
+
+            attention = F.softmax(scores / self.head_dim ** 0.5, dim = -1)
+
+            final_context = attention @ V
+            contexts.append(final_context)
+
+        multihead_context = torch.cat(contexts, dim = -1)
+        output = multihead_context.matmul(self.w_o)
+        return output
+
+embed = nn.Embedding(num_embeddings=8000, embedding_dim=512)
+embeddings = embed(token_ids)
+embeddings = embeddings[:32]
+encodings = PositionalEncodings(49,512)
+position_encodes = encodings.forward(embeddings)
+print(position_encodes.shape)
+
+masked_multi = MaskedMHA(8,512,49)
+out = masked_multi.forward(position_encodes)
+print(out.shape)
+
